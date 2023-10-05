7d7358c81f1da2d9ffdc03302cc103204761673d
Greeny <verdepertuz07@gmail.com>
2023-10-05 12:26:58 -0500

    first commit

---
+++ interaction.js
@@ -0,0 +1,17 @@
+let button_1 = document.getElementById("b1");
+
+button_1.addEventListener("click", changeScene);
+
+let activeScene = 1;
+function changeScene() {
+  activeScene++;
+  console.log(activeScene);
+
+  let img_1 = document.getElementById("img_1");
+  let src_1 = "./img/s" + activeScene + ".png";
+  img_1.src = src_1;
+
+  let img_2 = document.getElementById("img_2");
+  let src_2 = "./img/p" + activeScene + ".png";
+  img_2.src = src_2;
+}
